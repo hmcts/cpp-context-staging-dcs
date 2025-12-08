bb31f68bd128999e0147a9ecda7390991c2d084b
zeldal-hmcts <zeldal.ozdemir1@hmcts.net>
2025-12-08 23:48:43 +0000

    PEG-2785 Migrate remaining contexts to Framework D

--- stagingdcs-integration-test/src/test/java/uk/gov/moj/cpp/staging/dcs/helper/QueryHelper.java
+++ stagingdcs-integration-test/src/test/java/uk/gov/moj/cpp/staging/dcs/helper/QueryHelper.java
@@ -12,49 +12,52 @@ import static uk.gov.justice.services.test.utils.core.http.RestPoller.poll;
 import static uk.gov.justice.services.test.utils.core.matchers.ResponsePayloadMatcher.payload;
 import static uk.gov.justice.services.test.utils.core.matchers.ResponseStatusMatcher.status;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 import org.hamcrest.Matcher;
+import uk.gov.justice.services.test.utils.core.http.FibonacciPollWithStartAndMax;
 
 public class QueryHelper {
 
     private static final String QUERY = "/stagingdcs-query-api/query/api/rest/stagingdcs/dcscase/cases/";
     private static final String TRANSACTION_METADATA_QUERY = "/stagingdcs-query-api/query/api/rest/stagingdcs/transaction-metadata/case/";
     private static final String TRANSACTION_DETAILS_QUERY = "/stagingdcs-query-api/query/api/rest/stagingdcs/transaction-detail";
-    private static final int QUERY_TIMEOUT_IN_SECONDS = 60;
+    private static final int QUERY_TIMEOUT_IN_SECONDS = 15;
+    public static final FibonacciPollWithStartAndMax POLL_INTERVAL = new FibonacciPollWithStartAndMax(Duration.ofMillis(30), Duration.ofMillis(300));
 
-    public void queryCaseDetailByCaseIdAndAssertMatch(final UUID caseId, final Matcher... matchers){
+    public void queryCaseDetailByCaseIdAndAssertMatch(final UUID caseId, final Matcher... matchers) {
         String url = getBaseUri() + QUERY + caseId.toString();
         poll(requestParams(url, "application/vnd.stagingdcs.query.dcscase-status-by-case-id+json")
-                .withHeader(USER_ID, randomUUID()))
-                .timeout(QUERY_TIMEOUT_IN_SECONDS, SECONDS)
+                        .withHeader(USER_ID, randomUUID()).build(),
+                POLL_INTERVAL, Duration.ofSeconds(QUERY_TIMEOUT_IN_SECONDS))
                 .until(status().is(OK), payload().isJson(allOf(matchers)));
     }
 
-    public String queryTransactionMetadataAndAssertMatch(final UUID caseId, final Map<String,String> params, final Matcher... matchers) {
+    public String queryTransactionMetadataAndAssertMatch(final UUID caseId, final Map<String, String> params, final Matcher... matchers) {
         String url = getBaseUri() + TRANSACTION_METADATA_QUERY + caseId.toString();
         if (isNotEmpty(params)) {
             url = url.concat("?");
-            for(String key : params.keySet()){
+            for (String key : params.keySet()) {
                 url = url.concat(key).concat("=").concat(params.get(key)).concat("&");
             }
             url = StringUtils.chop(url);
         }
         return poll(requestParams(url, "application/vnd.stagingdcs.query.transaction-metadata-for-case+json")
-                .withHeader(USER_ID, randomUUID()))
-                .timeout(QUERY_TIMEOUT_IN_SECONDS, SECONDS)
+                        .withHeader(USER_ID, randomUUID()).build(),
+                POLL_INTERVAL, Duration.ofSeconds(QUERY_TIMEOUT_IN_SECONDS))
                 .until(status().is(OK), payload().isJson(allOf(matchers)))
                 .getPayload();
     }
 
     public String queryTransactionDetailsAndAssertMatch(final List<UUID> tranIds, final Matcher... matchers) {
-        String url = getBaseUri() + TRANSACTION_DETAILS_QUERY.concat("?transactionIds=").concat(StringUtils.join(tranIds,","));
+        String url = getBaseUri() + TRANSACTION_DETAILS_QUERY.concat("?transactionIds=").concat(StringUtils.join(tranIds, ","));
         return poll(requestParams(url, "application/vnd.stagingdcs.query.transaction-detail+json")
-                .withHeader(USER_ID, randomUUID()))
-                .timeout(QUERY_TIMEOUT_IN_SECONDS, SECONDS)
+                        .withHeader(USER_ID, randomUUID()).build(),
+                POLL_INTERVAL, Duration.ofSeconds(QUERY_TIMEOUT_IN_SECONDS))
                 .until(status().is(OK), payload().isJson(allOf(matchers)))
                 .getPayload();
     }
